d4d36a2b353e6c90bbdcc183ca4ba931b34267f0
Redis <fkmzyc1@gmail.com>
2025-12-01 04:25:16 +0400

    Just add little clear-code in test_home_page

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -3,11 +3,7 @@ from pages.home_page import HomePage
 from pages.log_in_page import LogInPage
 
 
-
 def test_auth_page_check(auth, header):
-
     header.open_home()
     header.click_log_in_page()
     auth.should_have_username_label()
-
-
