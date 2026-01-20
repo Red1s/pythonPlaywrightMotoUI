8a84c6b67dd310f634dff324bd5a141ab90999b0
Red1s <88196399+Red1s@users.noreply.github.com>
2026-01-20 21:39:27 +0000

    Deploying to gh-pages from @ Red1s/pythonPlaywrightMotoUI@2812e14aa59950809f4fd86d24ab83c96a7125c6 🚀

--- pages/home_page.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import Page, expect
-from .base_page import BasePage, BASE_URL
-from .log_in_page import LogInPage
-
-
-class HomePage(BasePage):
-
-    def open_home(self):
-        self.open("/")
--- settings.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-LOGIN = os.environ["LOGIN"]
-PASSWORD = os.environ["PASSWORD"]
-BASE_URL = os.environ["BASE_URL"]
-EMAIL_FOR_SEND_TEST = os.environ["EMAIL_FOR_SEND_TEST"]
